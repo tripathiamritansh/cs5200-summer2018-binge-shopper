1105712660a2e5db8f7c40e3ee0c1e9241a41e7b
Vidit Gupta <vidit92.gupta@gmail.com>
2018-08-18 09:12:48 -0400

    getAllUsers(), deleteUser() and updateApproveUser() functions added

--- cs5200-summer2018-binge-shopper/src/main/java/edu/northeastern/cs5200/service/UserService.java
+++ cs5200-summer2018-binge-shopper/src/main/java/edu/northeastern/cs5200/service/UserService.java
@@ -1,10 +1,13 @@
 package edu.northeastern.cs5200.service;
 
 import edu.northeastern.cs5200.entity.UserEntity;
+import edu.northeastern.cs5200.exception.NotFoundException;
 import edu.northeastern.cs5200.repository.UserRepository;
 import org.mindrot.jbcrypt.BCrypt;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class UserService {
@@ -18,6 +21,18 @@ public class UserService {
 
     public UserEntity getUserById(int userId) {
         return userRepository.findById(userId);
+    }
+
+    public List<UserEntity> findAllUsers() {
+        return (List<UserEntity>) userRepository.findAll();
+    }
+
+    public UserEntity updateApproveUser(int userId, boolean status) {
+        UserEntity u = userRepository.findById(userId);
+        if(u == null)
+            throw new NotFoundException("User not found!");
+        u.setApproved(status);
+        return userRepository.save(u);
     }
 
     public UserEntity findUserByUsername(String username) {
